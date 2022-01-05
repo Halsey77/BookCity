c8213dd870d79222c9850f7d7a75137bd4f96f79
Halsey77 <provcc143300@gmail.com>
2022-01-05 15:59:31 +0700

    Thêm trang chủ

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+const homepageRouter = require('./components/homepage/homepageRouter');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', [path.join(__dirname, 'views')
+                  ,path.join(__dirname, 'components/homepage')]);
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -20,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Router setup
-app.use('/', indexRouter);
+app.use('/', homepageRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
