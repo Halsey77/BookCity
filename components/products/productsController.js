1e4de2c8d5b3cbba92d196d22663366907b599ee
Halsey77 <provcc143300@gmail.com>
2022-01-06 15:08:05 +0700

    Thêm products controller, router và service

---
+++ components/products/productsController.js
@@ -0,0 +1,11 @@
+const productsService = require('./productsService');
+
+exports.list = async (req, res) => {
+    const products = await productsService.list(!isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1:0);
+    res.render('productsList', {products: products});
+}
+
+exports.showDetail = async (req, res) => {
+    const productDetail = await productsService.showDetail(req.query.ID)
+    res.render('products/productdetail', {productDetail: productDetail});
+}
---
+++ components/products/productsService.js
@@ -0,0 +1,11 @@
+const {models} = require('../../models/index');
+const sach = models.sach;
+const { Op } = require("sequelize");
+
+exports.list = (page = 0, itemPerPage = 9) => {
+    return sach.findAll({
+        offset: page * itemPerPage,
+        limit: itemPerPage,
+        raw: true
+    });
+}
